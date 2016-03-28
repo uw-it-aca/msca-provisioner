a137cbd4fd7769edbaffdc442dc04e3e7fa2d7bd
mike seibel <mikes@washington.edu>
2016-03-28 16:19:00 -0700

    adds mysql connector via environment

--- msca_provisioner/settings.py
+++ msca_provisioner/settings.py
@@ -63,8 +63,12 @@ WSGI_APPLICATION = 'msca_provisioner.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ['DJANGO_DB_NAME'],
+        'USER': os.environ['DJANGO_DB_USER'],
+        'PASSWORD': os.environ['DJANGO_DB_PASSWORD'],
+        'HOST': os.environ['DJANGO_DB_HOST'],
+        'PORT': '',
     }
 }
 
