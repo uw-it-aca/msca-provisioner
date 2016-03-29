843934ea13fe78db7bef4a46cfeffadd1e676e1f
mike seibel <mikes@washington.edu>
2016-03-28 17:08:13 -0700

    adjusts urls.py

--- msca_provisioner/urls.py
+++ msca_provisioner/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
-    url(r'^$', 'provisioner.views', name='home'),
+    url(r'^$', 'provisioner.views.home', name='home'),
     #url(r'^admin/', include(admin.site.urls)),
 )
